b34674746898a9350be86692b08da5b8fcb232a0
Luke Karrys <lukekarrys@gmail.com>
2012-02-17 17:43:15 -0700

    Add background image change onhashchange

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,8 +1,28 @@
 /* Author:
 
 */
+(function($) {
 
+  var v = {
+    'canyon': {position: 'top left'},
+    'waterfall': {position: 'bottom left'},
+    'jump': {position: 'top center'}
+  };
 
+$(function() {
+  $(window).hashchange( function() {
+    var k = location.hash.replace('#', '').toLowerCase();
+    if (typeof v[k] !== undefined) {
+      $('html').css({
+        'background-image': 'url(../img/'+k+'.jpg)',
+        'background-position': v[k].position
+      });
+    } 
+  });
+  
+  $(window).hashchange();
+});
 
+})(jQuery);
 
 
