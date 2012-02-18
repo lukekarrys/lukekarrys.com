60a330f5fb1ce90d491d4012325202192791e39b
Luke Karrys <lukekarrys@gmail.com>
2012-02-17 17:09:45 -0700

    add simple deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+#
+# An example hook script that is called after a successful
+# commit is made.
+#
+# To enable this hook, rename this file to "post-commit".
+
+scp -r public_html/* linode:www/lukekarrys.com/public_html/
