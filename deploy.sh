e9754bd3ccd49f07357f566d80c5a40b8d10f8f9
Luke Karrys <lukekarrys@gmail.com>
2012-02-17 23:42:34 -0700

    Use rsync for deploy, duh

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@
 # To enable this hook, rename this file to "post-commit".
 
 scp -r public_html/* linode:www/lukekarrys.com/public_html/
+
+rsync -avze 'ssh -p linode' --delete public_html/ linode:www/lukekarrys.com/public_html/
