fcebe37267e5d52e2a4843b4e53e5912dcf65558
Luke Karrys <lukekarrys@gmail.com>
2012-02-17 23:44:03 -0700

    Use rsync for deploy, duh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-#
-# An example hook script that is called after a successful
-# commit is made.
-#
-# To enable this hook, rename this file to "post-commit".
 
-scp -r public_html/* linode:www/lukekarrys.com/public_html/
-
-rsync -avze 'ssh -p linode' --delete public_html/ linode:www/lukekarrys.com/public_html/
+rsync -avze 'ssh -p 22' --delete public_html/ linode:www/lukekarrys.com/public_html/
