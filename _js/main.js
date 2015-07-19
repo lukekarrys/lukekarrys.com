adff09801b4f3158478cbc74d4a9647052c97453
Luke Karrys <lukekarrys@gmail.com>
2015-07-18 21:18:37 -0700

    code, padding, typos, content, etc

--- _js/main.js
+++ _js/main.js
@@ -2,15 +2,28 @@ require('waypoints/lib/noframework.waypoints');
 require('waypoints/lib/shortcuts/inview');
 
 var dom = require('ampersand-dom');
+var modules = document.getElementById('modules');
 var footer = document.getElementById('footer');
-var background = document.getElementById('background-blur');
+var blur = document.getElementById('background-blur');
+var background = document.getElementById('background');
 
+// When the top of #modules (the last section) starts to get scrolled
+// out of view, then load the non-blurred bg image
+new Waypoint.Inview({
+    element: modules,
+    exit: function () {
+        background.style['background-image'] = 'url(/img/bg.jpg)';
+    }
+});
+
+// When the footer enters and exits the viewport,
+// fade out the burred background image
 new Waypoint.Inview({
     element: footer,
     enter: function () {
-        dom.addClass(background, 'clear');
+        dom.addClass(blur, 'clear');
     },
     exited: function () {
-        dom.removeClass(background, 'clear');
+        dom.removeClass(blur, 'clear');
     }
 });
